ac7d2baf43c06768fcf8ce82bda98504a8f8dcde
jhemi-cristina <jhemilycristina@gmail.com>
2022-02-01 15:53:06 -0300

    app.js update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './calculadora.css';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Jumbotron style={{
+      background: 'transparent !important',
+      backgroundColor: '#007bff',
+      padding: '5px',
+      margin: '5px',
+      width: '400px'
+          }}>
+    </Jumbotron>
   );
 }
 
