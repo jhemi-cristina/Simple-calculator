1f590984f0b07e4992ab5d87cd369e7759fa08f7
jhemi-cristina <jhemilycristina@gmail.com>
2022-02-07 14:23:54 -0300

    test creation

---
+++ src/calculadora.service.js
@@ -0,0 +1,33 @@
+function CalculadoraService() {
+  const SOMA = "+";
+  const SUBTRACAO = "-";
+  const DIVISAO = "/";
+  const MULTIPLICACAO = "*";
+
+  function calcular(numero1, numero2, operacao) {
+    let resultado;
+
+    // eslint-disable-next-line default-case
+    switch (operacao) {
+      case SOMA:
+        resultado = numero1 + numero2;
+        break;
+      case SUBTRACAO:
+        resultado = numero1 - numero2;
+        break;
+      case DIVISAO:
+        resultado = numero1 / numero2;
+        break;
+      case MULTIPLICACAO:
+        resultado = numero1 * numero2;
+        break;
+      default:
+        resultado = 0;
+    }
+    return resultado;
+  }
+
+  return [calcular, SOMA, SUBTRACAO, DIVISAO, MULTIPLICACAO];
+}
+
+export default CalculadoraService;
